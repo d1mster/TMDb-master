2cd3a9d56a93804ea28b52bffcb81e0a4be3dd54
Dmitriy Pak <thx.cap.674@gmail.com>
2018-10-17 17:00:48 +0600

    Initial Commit

---
+++ TMDb/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TMDb
+//
+//  Created by Dmitriy Pak on 10/17/18.
+//  Copyright © 2018 Dmitriy Pak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
